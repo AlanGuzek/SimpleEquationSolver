e426b5b465dbf20d583db12c524105a9e1c170d2
AlanGuzek <alanguzek@student.agh.edu.pl>
2021-01-20 22:41:04 +0100

    Oh Yeah

--- dataset.py
+++ dataset.py
@@ -2,20 +2,25 @@ from PIL import Image
 import numpy as np
 import pandas as pd
 import os
-import csv
 
 
-dataset = []
-value = []
-path = 'resizedtrain'
-for folder in os.listdir(path):
-    for character in os.listdir(os.path.join(path, folder)):
-        img = np.asarray(Image.open(os.path.join(path, folder, character)).convert('L'))
-        img = img.flatten()
-        dataset.append(img)
-        value.append(folder)
-dataset = np.asarray(dataset, dtype='object')
-df = pd.DataFrame({"Data": dataset, "Value": value})
-df.to_csv('dataset.csv')
+def get_dataset():
+    dataset = []
+    value = []
+    path = 'resizedtrain'
+    i = 0
+    names = {}
+    for folder in os.listdir(path):
+        names[folder] = i
+        for character in os.listdir(os.path.join(path, folder)):
+            img = np.asarray(Image.open(os.path.join(path, folder, character)).convert('L'))
+            img = img.flatten()
+            dataset.append(img)
+            value.append(i)
+        i += 1
+    df = pd.DataFrame(dataset)
+    df['Values'] = value
+    return df
 
-pass
+
+get_dataset()
