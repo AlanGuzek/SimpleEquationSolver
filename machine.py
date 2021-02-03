2817609d46a8ab620ed5c379885c20ebd60142a1
AlanGuzek <alanguzek@student.agh.edu.pl>
2021-02-03 20:39:39 +0100

    SVM needs really low C parameter Don't know how to make neural network work better Trying to make at least 90% accuracy score Image resizing and preparing to predicting is ready

--- machine.py
+++ machine.py
@@ -1,22 +1,40 @@
+# Making a model that will recognize digits and arithmetic signs from pixel-dataframe
+
+# Importing
 from sklearn.svm import SVC
-from sklearn.datasets import load_digits
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
 import pickle
 import pandas as pd
 import numpy as np
-from dataset import get_dataset
-from sklearn.datasets import load_digits
 
 
-data = get_dataset()
-X = data
-y = X.drop(X.columns[['Values']], inplace=True)
+# Preparing Data
+data = pd.read_pickle("signs_data.pkl")
+pca = PCA(n_components=1200)
+scaler = StandardScaler()
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+# X = pca.fit_transform(X, y)
+scaler.fit(X=X)
+# X = scaler.transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y)
 
+# Making model using SVM
 
-svm = SVC(C=2, kernel='poly')
+svm = SVC(C=0.05, kernel='poly')
 svm.fit(X_train, y_train)
 y_pred = svm.predict(X_test)
-print(accuracy_score(y_test, y_pred))
+print("SVC accuracy = " + str(accuracy_score(y_test, y_pred)))
+
+
+# Making model using NNC
+# nnc = MLPClassifier(activation='relu', solver='sgd', learning_rate='adaptive')
+nnc = MLPClassifier(solver='lbfgs', max_iter=10000)
+nnc.fit(X_train, y_train)
+y_pred = nnc.predict(X_test)
+print("NN accuracy = " + str(accuracy_score(y_test, y_pred)))
