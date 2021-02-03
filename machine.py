233c86267530811ce71523154469456be3e7a102
AlanGuzek <alanguzek@student.agh.edu.pl>
2021-02-03 21:37:41 +0100

    Ready to testing machines

--- machine.py
+++ machine.py
@@ -26,7 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15)
 
 # Making model using SVM
 
-svm = SVC(C=0.05, kernel='poly')
+svm = SVC(C=0.023, kernel='poly')
 svm.fit(X_train, y_train)
 y_pred = svm.predict(X_test)
 print("SVC accuracy = " + str(accuracy_score(y_test, y_pred)))
@@ -34,7 +34,7 @@ print("SVC accuracy = " + str(accuracy_score(y_test, y_pred)))
 
 # Making model using NNC
 # nnc = MLPClassifier(activation='relu', solver='sgd', learning_rate='adaptive')
-nnc = MLPClassifier(solver='lbfgs', max_iter=10000)
-nnc.fit(X_train, y_train)
-y_pred = nnc.predict(X_test)
-print("NN accuracy = " + str(accuracy_score(y_test, y_pred)))
+# # nnc = MLPClassifier(solver='lbfgs', max_iter=3000)
+# nnc.fit(X_train, y_train)
+# y_pred = nnc.predict(X_test)
+# print("NN accuracy = " + str(accuracy_score(y_test, y_pred)))
