e7c1c950e9601d3f3a2d9d8985b3cb1baa845208
AlanGuzek <alanguzek@student.agh.edu.pl>
2021-02-03 20:47:38 +0100

    copying files

--- dataset.py
+++ dataset.py
@@ -6,21 +6,23 @@ import os
 
 def get_dataset():
     dataset = []
-    value = []
     path = 'resizedtrain'
     i = 0
-    names = {}
+    names: dict = {}
     for folder in os.listdir(path):
         names[folder] = i
+        i += 1
         for character in os.listdir(os.path.join(path, folder)):
             img = np.asarray(Image.open(os.path.join(path, folder, character)).convert('L'))
             img = img.flatten()
-            dataset.append(img)
-            value.append(i)
-        i += 1
+            temp: list = []
+            for b in img:
+                temp.append(b)
+            temp.append(i)
+            dataset.append(temp)
     df = pd.DataFrame(dataset)
-    df['Values'] = value
+    df.to_pickle("data_frame.pkl")
     return df
 
 
-get_dataset()
+collected_data = get_dataset()
